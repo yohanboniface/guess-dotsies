2f0b8e9a312111f3bcaf8ddf8fb5be90040a2ef4
Yohan Boniface <yb@enix.org>
2012-07-24 13:01:42 +0200

    Add an English tool words corpus

---
+++ src/js/corpus/tool_words.js
@@ -0,0 +1,14 @@
+define([], function(){ 
+    return {
+        "name": "Tool words",
+        "lang": "en",
+        "elements": [
+            'is', 'the', 'in', 'be', 'are', 'you', 'she', 'he', 'us',
+            'they', 'me', 'my', 'it', 'been', 'or', 'and', 'but', 'here',
+            'how', 'lot', 'a', 'I', 'we', 'her', 'to', 'was', 'of', 'with',
+            'as', 'for', 'on', 'that', 'this', 'in', 'at', 'yes','no', 'by',
+            'what'
+        ],
+    }
+;
+});
